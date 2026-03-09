2793fa00f09834e06348dbb14a1f2ad0b320b9bf
Li Tan <robloxcity88@gmail.com>
2026-03-08 19:51:17 -0600

    Worked on implenting multi-threading in GetAccess. Still need to fix the updating of the true anomaly. Consider just not updating it at all but starting at intervals where we know the true anomaly is the same.

--- Optical Downlink/GetAccess.m
+++ Optical Downlink/GetAccess.m
@@ -1,45 +1,64 @@
 clear
 close all
 
+% SOME ISSUES:
+% Calculating current true anomaly is not accurate!
+% Consider splitting the time interval into orbital periods so we know
+% true anomaly will be the same
+
 TOTAL_TIME_HR = 8760;
 ONE_ITERATION_HR = 20;
 SAMPLE_TIME = 0.5;        % determines length of time intervals (seconds)
-OUTPUT_FILE = "DurationsTable_1YR.csv";
-currentDate = datetime(2025,3,24,0,0,0); % Initial value is start
+OUTPUT_FILE = "DurationsTable_1YR_Test.csv";
+START_DATE = datetime(2025,3,24,0,0,0);  % Initial value
 
+initialTableSize = 30;      % can adjust to speed up program
+
+% Initial orbital elements
 semiMajorAxis = 6950440;
 eccentricity = 1e-6;
 inclination = 98; 
-RAAN = 135.58;
+RAANi = 135.58;                         
 argOfPeriapsis = 0;
 trueAnomaly = 1.81165e-15;              % Will change during orbit
 
-% 1st column contains date, 2nd contains duration
-durations = table('Size', [100, 2], 'VariableTypes', ...
-    {'datetime', 'int8'}, 'VariableNames', {'Start Date (UTC)', 'Duration (s)'});
-
-currentCell = {};
-currentIndex = 1;
-
- % Init scenario
-stopTime = currentDate + hours(ONE_ITERATION_HR);
-sc = satelliteScenario(currentDate,stopTime,SAMPLE_TIME);
-
-% Visualize field of view of sensor
-% satelliteScenarioViewer(sc);               % Uncomment to open simulation
+% Visualize field of view of sensor (Uncomment to open simulation)
+% satelliteScenarioViewer(sc);     
 % fieldOfView(camSensor);
 
-% Rothney station
-gs = groundStation(sc, Name="Rothney Station", Latitude=50.868, Longitude=-114.291);
+parfor i = 0:(TOTAL_TIME_HR / ONE_ITERATION_HR)
+    % Init scenario
+    currentDate = START_DATE + i * hours(ONE_ITERATION_HR);    % One iteration in hours
+    stopTime = currentDate + hours(ONE_ITERATION_HR);
+    sc = satelliteScenario(currentDate,stopTime,SAMPLE_TIME);
 
-% Re-add satellite
-[ac, CtS] = AddSatellite(sc, semiMajorAxis, eccentricity, inclination, ...
-    RAAN, argOfPeriapsis, trueAnomaly, gs);
+    % Rothney station
+    gs = groundStation(sc, Name="Rothney Station", Latitude=50.868, Longitude=-114.291);
 
-for i = 0:1:(TOTAL_TIME_HR / ONE_ITERATION_HR)
+    % Get the current true anomaly
+    [r, v] = propagateOrbit(currentDate, semiMajorAxis, eccentricity, ...
+                inclination, RAANi, argOfPeriapsis, trueAnomaly);
+        
+    [a, ecc, incl, RAAN, argp, nu, truelon, arglat, lonper] = ijk2keplerian(r,v);
+
+    % Initialize CtS satellite with updated orbit parameters
+    CtS = satellite(sc, semiMajorAxis, eccentricity, inclination, ...
+    RAANi, argOfPeriapsis, nu, Visual3DModel="NarrowBodyAirliner.glb", ...
+    Name="CtS", OrbitPropagator="two-body-keplerian");
+    
+    % Conical sensor   
+    camSensor = conicalSensor(CtS, 'Name', "Antenna", MaxViewAngle=7, MountingAngles=[0;0;0]); % yaw, pitch, roll
+    ac = access(camSensor, gs);
+
     % 1 x num of time intervals, contains boolean values
     accessIntervals = accessStatus(ac);
     accessSize = numel(accessIntervals);
+
+    % Set up containers for data
+    currentCell = {};
+    tempData = table('Size', [initialTableSize, 2], 'VariableTypes', ...
+    {'datetime', 'int8'}, 'VariableNames', {'Start Date (UTC)', 'Duration (s)'});
+    currentIndex = 1;
 
     % Add all durations
     for j = 1:accessSize
@@ -47,9 +66,10 @@ for i = 0:1:(TOTAL_TIME_HR / ONE_ITERATION_HR)
         if accessIntervals(j) && j > 1
             % If previous entry is false, start new duration
             if ~accessIntervals(j - 1)
+                
                 % Add any previous cell
                 if numel(currentCell) ~= 0
-                    durations(currentIndex, :) = currentCell;
+                    tempData(currentIndex, :) = currentCell;
 
                     currentIndex = currentIndex + 1;
                 end
@@ -63,25 +83,22 @@ for i = 0:1:(TOTAL_TIME_HR / ONE_ITERATION_HR)
         end
     end
 
-    currentDate = currentDate + seconds(accessSize * SAMPLE_TIME);
-    
-    % Update true anomaly
-    trueAnomaly = orbitalElements(CtS).TrueAnomaly;
+    % Add last cell to durations if possible
+    if (numel(currentCell) ~= 0)
+        tempData(currentIndex, :) = currentCell;
+    end
 
-    % Update scenario
-    sc.StartTime = currentDate;
-    sc.StopTime = currentDate + hours(ONE_ITERATION_HR);
+    % Removes all missing entries
+    tempData = tempData(~ismissing(tempData(:,1)), :);
 
-    delete(sc.Satellites)
-
-    % Re-add satellite
-    [ac, CtS] = AddSatellite(sc, semiMajorAxis, eccentricity, inclination, ...
-        RAAN, argOfPeriapsis, trueAnomaly, gs);
+    % Only write if there is atleast one entry
+    if (numel(tempData) ~= 0)
+        writetable(tempData, OUTPUT_FILE, 'WriteMode', 'append');
+    end
 end
 
-% Add last cell to durations
-durations(currentIndex, :) = currentCell;
 
+%{
 % Convert to seconds
 durations(:,2) = durations(:,2) .* SAMPLE_TIME;
 
@@ -93,6 +110,11 @@ meanDuration = meanDuration{1,1} / currentIndex;
 
 % Save the durations table to a CSV file
 writetable(durations(1:currentIndex, :), OUTPUT_FILE);
+
+table('Size', [100, 2], 'VariableTypes', ...
+    {'datetime', 'int8'}, 'VariableNames', {'Start Date (UTC)', 'Duration (s)'})
+%}
+
 
 
 
